94b1210d2c82154c695362a8526e77b9c2d9ebb0
rahulrr-coder <rahulrr5002@protonmail.com>
2025-02-26 11:52:13 +0530

    Product card with button

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,11 @@
-// write button card here
+import React from "react";
+
+const ViewProductButton = () => {
+  return (
+    <button className="mt-3 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+      View Product
+    </button>
+  );
+};
+
+export default ViewProductButton;
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,21 @@
-// write product card here
+import React from "react";
+import ViewProductButton from "../components/button";
+
+const Productcard = () => {
+
+  const productImage = "https://shorturl.at/MgY6M";
+  const productName = "Casual Shoes";
+  const productPrice = "$50";
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-4 w-64">
+      <img src={productImage} alt={productName} className="w-20 h-20 object-cover rounded" />
+      <h2 className="text-lg font-semibold mt-2">{productName}</h2>
+      <p className="text-gray-600">{productPrice}</p>
+      <ViewProductButton />
+    </div>
+  );
+};
+
+export default Productcard;
+
